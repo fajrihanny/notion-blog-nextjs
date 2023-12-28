a143d13f40dded1c5a029ab2fc5b525a70b87b15
Fajri Hanny <fajri.hanny@gmail.com>
2023-12-28 16:49:09 +0100

    Modified index.js

--- pages/index.js
+++ pages/index.js
@@ -10,13 +10,14 @@ export default function Home({ posts }) {
   return (
     <div>
       <Head>
-        <title>Notion Next.js blog</title>
+        <title>Fanny's Blog on Notion</title>
+        {/* Title on the Blog - not on the page itself */}
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.container}>
         <header className={styles.header}>
-          <div className={styles.logos}>
+{/*           <div className={styles.logos}>
             <svg
               height="80"
               width="80"
@@ -46,25 +47,14 @@ export default function Home({ posts }) {
                 ></path>
               </g>
             </svg>
-          </div>
-          <h1>Next.js blog powered by Notion API</h1>
+          </div> */}
+          <h1>Fanny's Blog on Notion</h1>
           <p>
-            This is an example of a Next.js blog with data fetched with Notions
-            API. The data comes from{" "}
-            <a href={`https://www.notion.so/${databaseId}`}>this table</a>. Get
-            the source code on{" "}
-            <a href="https://github.com/samuelkraft/notion-blog-nextjs">
-              Github
-            </a>{" "}
-            or read{" "}
-            <a href="https://samuelkraft.com/blog/building-a-notion-blog-with-public-api">
-              my blogpost
-            </a>{" "}
-            on building your own.
+            Musing thoughts
           </p>
         </header>
 
-        <h2 className={styles.heading}>All Posts</h2>
+        <h2 className={styles.heading}>All Blog Posts</h2>
         <ol className={styles.posts}>
           {posts.map((post) => {
             const date = new Date(post.last_edited_time).toLocaleString(
