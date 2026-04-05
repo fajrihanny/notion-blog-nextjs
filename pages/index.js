bfcf153f919a8650bf1e04ab7863a822a6f81567
Fajri Hanny <fajri.hanny@gmail.com>
2026-04-06 01:07:29 +0200

    Added summary and featured image

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,7 @@ import styles from "./index.module.css";
 
 export const databaseId = process.env.NOTION_DATABASE_ID;
 
+
 export default function Home({ posts }) {
   return (
     <div>
@@ -17,34 +18,31 @@ export default function Home({ posts }) {
 
       <main className={styles.container}>
         <header className={styles.header}>
-          <h1>Fanny's Mini Blog</h1>
+          <h1>Fanny's Review</h1>
           <p>
-            I write stuffs.
+            I write stuffs - sometimes.
           </p>
         </header>
 
-        <h2 className={styles.heading}>What do I write</h2>
+        <h1 className={styles.heading}>I think and I write</h1>
         <ol className={styles.posts}>
           {posts.map((post) => {
-            //console.log(post);
-            //console.log("Published "+post.properties.Published.checkbox);
-            //console.log("Slug "+post.properties.Slug.rich_text[0].text.content);
-            //console.log("Tags "+post.properties.Tags.multi_select[0].name);
-            //console.log("Image URL: "+post.properties.Image.url);
-            //console.log(Object.entries(post.properties.Blurbs.rich_text));
-
-            //const blurbs = post.properties.Blurbs.rich_text[0].text;
-            const created_date = new Date(post.created_time).toLocaleString(
+          console.log(post);
+          const tagscount = (post.properties.Tags.multi_select).length;
+          console.log(tagscount);
+          for (let index = 0; index < tagscount; index++) {
+            console.log(post.properties.Tags.multi_select[index].name);
+          }
+            const published_date = new Date(post.properties.Date.date.start).toLocaleString(
               "en-GB",
               {
                 day: "numeric",
-                month: "numeric",
+                month: "short",
                 year: "numeric",
-                weekday: "short"
               },
             );
-            
-            
+
+            const src = post.properties.Image.files[0].type === "external" ? post.properties.Image.files[0].external.url : post.properties.Image.files[0].file.url;
             return (
               <li key={post.id} className={styles.post}>
                 <h3 className={styles.postTitle}>
@@ -52,7 +50,10 @@ export default function Home({ posts }) {
                     <Text text={post.properties.Name.title} />
                   </Link>
                 </h3>
-                <p className={styles.postDescription}>{created_date}</p>
+                <p className={styles.postDescription}>{published_date}</p>
+                <p><img className={styles.featuredImg} src={src}/>    </p>
+                <p className={styles.postSummary}>{(post.properties.Summary.rich_text[0].text.content)}</p>  
+                
                 <Link href={`/${post.id}`}>Read full post →</Link>
               </li>
             );
